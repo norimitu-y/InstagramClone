b04cd81d3cf94dc0d4c28d066ef4d42b83af5dce
安井 教光 <norimitu@iMac.local>
2018-12-24 12:36:00 +0900

    createしない

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,30 @@
+class PicturesController < ApplicationController
+  def index
+    @pictures = Picture.all
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    Picture.create(picture_params)
+    #redirect_to new_picture_path
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:title, :content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
 
 root to: "top#index"
 
-resources :pictures do
-  collection do
-    post :confirm
-  end
-end
+resources :pictures 
 
 end
